8787b76899da21a163448dcc1d6ab6025d66e49d
Ofek Kfir <ofek.kfir@gmail.com>
2018-06-11 00:18:38 +0300

    some small changes

--- Yalla project/main.asm
+++ Yalla project/main.asm
@@ -8,13 +8,10 @@ include \masm32\include\winmm.inc
 includelib \masm32\lib\winmm.lib 
 .code
  ; TODO: finish jump(V), add cactus, maybe add score, replace sleep with count(V), 
- ; Do a sperate count for dino and the rest (v), findout why sound is in delay
+ ; Do a sperate count for dino and the rest (v), Do a func that would draw the cactus in random
+ ; paces and cactus x Would go with speed game
 main proc
 invoke init
-
-
-
-
 loopa:
 
 invoke update
