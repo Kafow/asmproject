181e310a183dd96fcd4962c757ca7932498b34ed
Ofek Kfir <ofek.kfir@gmail.com>
2018-06-17 01:30:58 +0300

    michsol

--- Yalla project/main.asm
+++ Yalla project/main.asm
@@ -7,12 +7,14 @@ include main_funcs.inc
 include \masm32\include\winmm.inc
 includelib \masm32\lib\winmm.lib 
 .code
- ; TODO: finish jump(V), add cactus, maybe add score, replace sleep with count(V), 
+ ; TODO: finish jump(V), add cactus(V), maybe add score, replace sleep with count(V), 
  ; Do a sperate count for dino and the rest (v), Do a func that would draw the cactus in random
- ; paces and cactus x Would go with speed game (Can do a func that will set cactus pos and put it on update)
+ ; paces and cactus x Would go with speed game (Can do with a func that calculates and activate CalcCactusPos based on the func) 
+ ;(Can do a func that will set cactus pos and put it on update(V)), can add gap by telling if he is not 1200- 150, dont activate func
 main proc
 invoke init
 loopa:
+
 
 invoke update
 invoke draw
