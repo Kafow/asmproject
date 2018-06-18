72e12be9cca3f4277158530a07527ee787dab005
Ofek Kfir <ofek.kfir@gmail.com>
2018-06-19 02:50:07 +0300

    With the book and comments

--- Yalla project/main.asm
+++ Yalla project/main.asm
@@ -7,17 +7,14 @@ include main_funcs.inc
 include \masm32\include\winmm.inc
 includelib \masm32\lib\winmm.lib 
 .code
- ; TODO: finish jump(V), add cactus(V), maybe add score, replace sleep with count(V), 
- ; Do a sperate count for dino and the rest (v), Do a func that would draw the cactus in random
- ; paces and cactus x Would go with speed game (Can do with a func that calculates and activate CalcCactusPos based on the func) 
- ;(Can do a func that will set cactus pos and put it on update(V)), can add gap by telling if he is not 1200- 150, dont activate func
+ 
 main proc
-invoke init
+invoke init ;openning the window and loading the images
 loopa:
 
-invoke restart, offset dinod
-invoke update
-invoke draw
+invoke restart, offset dinod ;check if need to restart
+invoke update ;update the loop and the funcs
+invoke draw ;drawing the game
 
 jmp loopa
 ret
